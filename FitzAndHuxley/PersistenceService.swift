c4eb82d4fe9f6f67b10ebd7ddb1ade8af312aeb8
Vanessa Balzer <vanessabalzer@vanessas-macbook-pro.local>
2018-12-14 09:28:36 +0100

    DataSourceClass

---
+++ FitzAndHuxley/PersistenceService.swift
@@ -0,0 +1,9 @@
+//
+//  PersistenceService.swift
+//  FitzAndHuxley
+//
+//  Created by Vanessa Balzer on 14.12.18.
+//  Copyright © 2018 Vanessa Balzer. All rights reserved.
+//
+
+import Foundation
