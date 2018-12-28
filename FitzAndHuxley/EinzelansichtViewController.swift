7e57966d2f3adbf2176b40efafb6165af0352d93
Vanessa Balzer <vanessabalzer@vanessas-mbp>
2018-12-28 22:22:04 +0100

    Einzelansicht Controller (paar AutoLayouts)

---
+++ FitzAndHuxley/EinzelansichtViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EinzelansichtViewController.swift
+//  FitzAndHuxley
+//
+//  Created by Vanessa Balzer on 28.12.18.
+//  Copyright © 2018 Vanessa Balzer. All rights reserved.
+//
+
+import UIKit
+
+class EinzelansichtViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
