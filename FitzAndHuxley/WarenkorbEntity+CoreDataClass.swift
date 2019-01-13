644ac5b819424060ae63cdfd0924d83055431f3d
Vanessa Balzer <vanessa.balzer@live.de>
2019-01-13 14:01:12 +0100

    NOCH EINMAL

---
+++ FitzAndHuxley/WarenkorbEntity+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  WarenkorbEntity+CoreDataClass.swift
+//  FitzAndHuxley
+//
+//  Created by Vanessa Balzer on 13.01.19.
+//  Copyright © 2019 Vanessa Balzer. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(WarenkorbEntity)
+public class WarenkorbEntity: NSManagedObject {
+
+}
