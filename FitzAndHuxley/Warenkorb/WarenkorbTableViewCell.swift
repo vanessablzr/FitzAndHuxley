4def71ded5cc0f16de93fbdcee4b2c1509b7cdda
Vanessa Balzer <vanessa.balzer@live.de>
2019-01-13 12:05:06 +0100

    blabla

--- FitzAndHuxley/Warenkorb/WarenkorbTableViewCell.swift
+++ FitzAndHuxley/Warenkorb/WarenkorbTableViewCell.swift
@@ -9,16 +9,25 @@
 import UIKit
 
 class WarenkorbTableViewCell: UITableViewCell {
-
+    
+//    Outlets
+    @IBOutlet weak var imageArtikel: UIImageView!
+    @IBOutlet weak var nameArtikel: UILabel!
+    @IBOutlet weak var loeschenButton: UIButton!
+    @IBOutlet weak var preisArtikel: UILabel!
+    @IBOutlet weak var artikelFarbe: UILabel!
+    @IBOutlet weak var artikelGroesse: UILabel!
+    @IBOutlet weak var artikelAnzahl: UILabel!
+    
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
     }
 
 }
